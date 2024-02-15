3131b05227ddcb45372f887e486cdb968b3eadd1
forazaraf <forazaraf@gmail.com>
2024-02-15 14:36:22 +0100

    First commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var card string = "Ace of Spades"
+	//tworzenie zmiennej / nazwa zmiennej /typ zmiennej
+	// card:= newCard()
+
+	// fmt.Println(card)
+
+	cards := deck{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	cards.print()
+
+	for i := 1; i <= len(cards); i++ {
+		fmt.Println(i, cards[i-1])
+	}
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
